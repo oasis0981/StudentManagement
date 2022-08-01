ef27b2e46b4d15ff23ab09e0190b386fb06bcbc3
DESKTOP-BJPK43B\user <hootingowl09@naver.com>
2022-08-01 13:46:06 +0900

    update

--- project1.py
+++ project1.py
@@ -2,7 +2,6 @@ f = open("C:/StudentFile/Students.txt")
 lines = f.readlines()
 f.close
 
-
 # keys = []
 # for i in range(5):
 #    keys.append(lines[i][0:8])
@@ -11,11 +10,14 @@ f.close
 StuData = []
 for i in range(5):
     StuData.append(lines[i].split())
+    StuData[i][1] = StuData[i][1] + ' ' + StuData[i][2]
+    del StuData[i][2]
+
 
 StuAvg = []
 StuAvg = list(map(float,StuAvg))
 for i in range(5):
-    StuAvg.append((int(StuData[i][3]) + int(StuData[i][4])) / 2)
+    StuAvg.append((int(StuData[i][2]) + int(StuData[i][3])) / 2)
     StuData[i].append(StuAvg[i])
 
 
@@ -33,18 +35,29 @@ for i in range(5):
         StuGrade.append('F')
     StuData[i].append(StuGrade[i])
 
+StuData.sort(reverse=True, key=lambda x:x[4])
 
-StuData.sort(reverse=True, key=lambda x:x[5])
-# print(StuData)
+strFormat = '%8s%15s%10s%10s%10s%10s'
+strOut = strFormat % ("Student", "Name", "Midterm", "Final", "Average", "Grade")
 
 
 a = input()
-item = "Student Name Midterm Final Average Grade"
-item.replace(" ", "\t")
-
-
 if a.upper() == 'SHOW':
-    print(item)
-    print("-" * 50)
+    print(strOut)
+    print("-" * 70)
     for i in range(5):
-        print('\t'.join(map(str, StuData[i])))
+        StuDataOut = strFormat % (StuData[i][0],StuData[i][1],StuData[i][2],StuData[i][3],StuData[i][4],StuData[i][5])
+        print(StuDataOut)
+
+
+if a.upper() == 'SEARCH':
+    StudentID = input("Student ID: ")
+    for i in range(5):
+        if StuData[i][0] == StudentID:
+            print(strOut)
+            print("-" * 70)
+            StuDataOut = strFormat % (StuData[i][0], StuData[i][1], StuData[i][2], StuData[i][3], StuData[i][4], StuData[i][5])
+            print(StuDataOut)
+        else:
+            print('NO SUCH PERSON.')
+            break
