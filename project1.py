18b38514ee2735424c6bb75a7c19624babb04f48
DESKTOP-BJPK43B\user <hootingowl09@naver.com>
2022-08-01 19:50:34 +0900

    search, changescore, add 기능 추가

--- project1.py
+++ project1.py
@@ -35,29 +35,126 @@ for i in range(5):
         StuGrade.append('F')
     StuData[i].append(StuGrade[i])
 
-StuData.sort(reverse=True, key=lambda x:x[4])
+
 
 strFormat = '%8s%15s%10s%10s%10s%10s'
 strOut = strFormat % ("Student", "Name", "Midterm", "Final", "Average", "Grade")
 
-
-a = input()
-if a.upper() == 'SHOW':
-    print(strOut)
-    print("-" * 70)
-    for i in range(5):
-        StuDataOut = strFormat % (StuData[i][0],StuData[i][1],StuData[i][2],StuData[i][3],StuData[i][4],StuData[i][5])
-        print(StuDataOut)
-
-
-if a.upper() == 'SEARCH':
-    StudentID = input("Student ID: ")
-    for i in range(5):
-        if StuData[i][0] == StudentID:
-            print(strOut)
-            print("-" * 70)
-            StuDataOut = strFormat % (StuData[i][0], StuData[i][1], StuData[i][2], StuData[i][3], StuData[i][4], StuData[i][5])
+while True:
+    a = input()
+    if a.upper() == 'SHOW':
+        StuData.sort(reverse=True, key=lambda x: x[4])
+        print(strOut)
+        print("-" * 70)
+        for i in range(len(StuData)):
+            StuDataOut = strFormat % (StuData[i][0],StuData[i][1],StuData[i][2],StuData[i][3],StuData[i][4],StuData[i][5])
             print(StuDataOut)
+
+
+    if a.upper() == 'SEARCH':
+        StudentID = input("Student ID: ")
+        for i in range(len(StuData)):
+            if StuData[i][0] == StudentID:
+                print(strOut)
+                print("-" * 70)
+                StuDataOut = strFormat % (StuData[i][0], StuData[i][1], StuData[i][2], StuData[i][3], StuData[i][4], StuData[i][5])
+                print(StuDataOut)
+                break
+        else:
+            print("NO SUCH STUDENT.")
+
+
+
+    if a.upper() == 'CHANGESCORE':
+        StudentID = input("Student ID: ")
+        for i in range(len(StuData)):
+            if StuData[i][0] == StudentID:
+                testType = input("Mid/Final? ")
+                if testType == 'mid':
+                    newScore = input("Input new score: ")
+                    if 0<= int(newScore) <=100:
+                        print(strOut)
+                        print("-" * 70)
+                        StuDataOut = strFormat % (StuData[i][0], StuData[i][1], StuData[i][2], StuData[i][3], StuData[i][4], StuData[i][5])
+                        print(StuDataOut)
+                        StuData[i][2] = newScore
+                        StuData[i][4] = (int(StuData[i][2]) + int(StuData[i][3])) / 2
+                        if StuData[i][4] >= 90:
+                            StuData[i][5] = 'A'
+                        elif StuData[i][4] >= 80:
+                            StuData[i][5] = 'B'
+                        elif StuData[i][4] >= 70:
+                            StuData[i][5] = 'C'
+                        elif StuData[i][4] >= 60:
+                            StuData[i][5] = 'D'
+                        else:
+                            StuData[i][5] = 'F'
+                        print("Score changed.")
+                        StuDataOut = strFormat % (StuData[i][0], StuData[i][1], StuData[i][2], StuData[i][3], StuData[i][4], StuData[i][5])
+                        print(StuDataOut)
+                elif testType == 'final':
+                    newScore = input("Input new score: ")
+                    if 0<= int(newScore) <=100:
+                        print(strOut)
+                        print("-" * 70)
+                        StuDataOut = strFormat % (StuData[i][0], StuData[i][1], StuData[i][2], StuData[i][3], StuData[i][4], StuData[i][5])
+                        print(StuDataOut)
+                        StuData[i][3] = newScore
+                        StuData[i][4] = (int(StuData[i][2]) + int(StuData[i][3])) / 2
+                        if StuData[i][4] >= 90:
+                            StuData[i][5] = 'A'
+                        elif StuData[i][4] >= 80:
+                            StuData[i][5] = 'B'
+                        elif StuData[i][4] >= 70:
+                            StuData[i][5] = 'C'
+                        elif StuData[i][4] >= 60:
+                            StuData[i][5] = 'D'
+                        else:
+                            StuData[i][5] = 'F'
+                        print("Score changed.")
+                        StuDataOut = strFormat % (StuData[i][0], StuData[i][1], StuData[i][2], StuData[i][3], StuData[i][4], StuData[i][5])
+                        print(StuDataOut)
+                break
         else:
             print('NO SUCH PERSON.')
-            break
+
+
+    if a.upper() == 'ADD':
+        NewStudentID = input("Student ID: ")
+        for i in range(len(StuData)):
+            if StuData[i][0] == NewStudentID:
+                print('ALREADY EXISTS.')
+                break
+        else:
+            NewName = input("Name: ")
+            NewMidScore = input("Midterm Score: ")
+            NewFinalScore = input("Final Score: ")
+            NewStudent = [NewStudentID, NewName, NewMidScore, NewFinalScore]
+            NewStudent.append((int(NewStudent[2]) + int(NewStudent[3])) / 2)
+            if NewStudent[4] >= 90:
+                NewStudent.append('A')
+            elif NewStudent[4] >= 80:
+                NewStudent.append('B')
+            elif NewStudent[4] >= 70:
+                NewStudent.append('C')
+            elif NewStudent[4] >= 60:
+                NewStudent.append('D')
+            else:
+                NewStudent.append('F')
+            print("Student added.")
+            StuData.append(NewStudent)
+
+    if a.upper() == 'SEARCHGRADE':
+        grade = ['A', 'B', 'C', 'D', 'F']
+        searchGrade = input("Grade to search: ")
+        for i in range(5):
+            if grade[i] == searchGrade:
+                print(strOut)
+                print("-" * 70)
+        for i in range(len(StuData)):
+            if StuData[i][5] == searchGrade:
+                StuSearchData = strFormat % (StuData[i][0], StuData[i][1], StuData[i][2], StuData[i][3], StuData[i][4], StuData[i][5])
+                print(StuSearchData)
+                continue
+            else:
+                print("NO SUCH GRADE")
